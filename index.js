0d0a2e4b21480e4dee33345080d8a9720512ab4c
Matthew Wolfe <thewolfeslayer@gmail.com>
2023-01-12 18:41:28 -0500

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your Code Here
+let userName = window.prompt('Please enter a username')
+let score = 0
+for (let i=0; i< questions.length; i++) {
+    let question = questions[i]
+    let answer = window.prompt(question.text)
+    if(answer === question.correctAnswer){
+        console.log('Correct')
+        score++
+    }
+
+}
+window.prompt('You got ' + score + ' out of ' + questions.length + 'questions correct')
